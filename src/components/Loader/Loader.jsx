62e91bb5e8ac7792beba69ecc5a26ff6b58721d5
Yuriy Kulakovskyi <84936189+yuriy-kulakovskyi@users.noreply.github.com>
2022-12-29 13:35:37 +0200

    Add files via upload

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -19,6 +19,35 @@ const Loader = () => {
 
     if (loader.current.style.opacity === "0") {
       loader.current.style.display = 'none';
+
+      setTimeout(() => {
+        gsap.from(".home__village", 1.2, {opacity: 0, y: 100, delay: .1});
+        gsap.to(".home__village", 1.2, {opacity: 1, y: 0, delay: .1});
+    
+        gsap.from(".home__pine", 1.2, {opacity: 0, y: 150, delay: .3});
+        gsap.to(".home__pine", 1.2, {opacity: 1, y: 0, delay: .3});
+    
+        gsap.from(".home__mountain-1", 1.2, {opacity: 0, y: 250, delay: .7});
+        gsap.to(".home__mountain-1", 1.2, {opacity: 1, y: 0, delay: .7});
+    
+        gsap.from(".home__mountain-2", 1.2, {opacity: 0, x: 150, delay: .5});
+        gsap.to(".home__mountain-2", 1.2, {opacity: 1, x: 0, delay: .5});
+    
+        gsap.from(".home__mountain-3", 1.2, {opacity: 0, x: -150, delay: .6});
+        gsap.to(".home__mountain-3", 1.2, {opacity: 1, x: 0, delay: .6});
+    
+        gsap.from(".home__moon", 1.2, {opacity: 0, y: 200, delay: .8});
+        gsap.to(".home__moon", 1.2, {opacity: 1, y: 0, delay: .8});
+    
+        gsap.from(".home__trineo", 1.2, {opacity: 0, x: -200, delay: .8});
+        gsap.to(".home__trineo", 1.2, {opacity: 1, x: 0, delay: .8});
+    
+        gsap.from(".home__title", 1.2, {opacity: 0, y: -60, delay: 1});
+        gsap.to(".home__title", 1.2, {opacity: 1, y: 0, delay: 1});
+
+        document.body.style.overflowY = "scroll";
+        document.querySelector(".center-xy").style.display = 'none';
+      }, 50);
     }
   }
   
